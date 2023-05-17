18b639575fa01b41ef4ebcbfe47b995c30fba28d
M.Spinks <mikespinks0401@gmail.com>
2023-05-17 13:07:57 -0500

    refactor: the header to TheHeader component

---
+++ src/lib/components/TheHeader.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+    import {AppBar} from "@skeletonlabs/skeleton"
+	const links = [
+		{ name: 'Home', to: '/' },
+		{ name: 'About', to: '/about' },
+		{ name: 'Contact', to: '/contact' }
+	];
+</script>
+
+
+		<div class="bg-surface-100-800-token">
+			<div class="container mx-auto">
+				<AppBar>
+					<svelte:fragment slot="lead">
+						<strong class="text-xl uppercase"><a href="/">FullStackMike</a></strong>
+					</svelte:fragment>
+					<svelte:fragment slot="default">
+						<ul class="flex justify-center gap-4 uppercase font-medium">
+							{#each links as link}
+							<li><a href={link.to}>{link.name}</a></li>	
+							{/each}
+						</ul>
+					</svelte:fragment>
+					<svelte:fragment slot="trail">
+						<a
+							class="btn btn-sm variant-ghost-surface"
+							href="https://www.linkedin.com/in/michael-spinks-254879201/"
+							target="_blank"
+							rel="noreferrer"
+						>
+							LinkedIn
+						</a>
+						<a
+							class="btn btn-sm variant-ghost-surface"
+							href="http://github.com/mikespinks0401"
+							target="_blank"
+							rel="noreferrer"
+						>
+							GitHub
+						</a>
+					</svelte:fragment>
+				</AppBar>
+			</div>
+		</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,40 +1,20 @@
 <script lang="ts">
 	// The ordering of these imports is critical to your app working properly
-	import '../theme.postcss'
+	import '../theme.postcss';
 	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import TheHeader from '$lib/components/TheHeader.svelte';
+
 </script>
 
 <!-- App Shell -->
 <AppShell>
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">FullStackMike</strong>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://www.linkedin.com/in/michael-spinks-254879201/"
-					target="_blank"
-					rel="noreferrer"
-				>
-					LinkedIn
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="http://github.com/mikespinks0401"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
-			</svelte:fragment>
-		</AppBar>
+		<TheHeader />
 	</svelte:fragment>
 	<!-- Page Route Content -->
 	<slot />
