060019fd2cfce1572de60e31369066af47f65c48
M.Spinks <mikespinks0401@gmail.com>
2023-05-17 11:50:31 -0500

    added port 3000 default & dev api proxy to 4500

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,12 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	server:{
+		port: 3000,
+		proxy: {'/api': {
+			target: 'http://localhost:4500',
+			rewrite: path => path.replace(/^\/api/, '')
+		}}
 	}
 });
