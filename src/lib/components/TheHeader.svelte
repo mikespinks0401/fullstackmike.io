0096b92bf7a9f33ea847cba05ca42aaf5c79bb41
M.Spinks <mikespinks0401@gmail.com>
2023-05-22 15:26:12 -0500

    todo: dynamic active classe on page

--- src/lib/components/TheHeader.svelte
+++ src/lib/components/TheHeader.svelte
@@ -2,16 +2,15 @@
 	import { browser } from '$app/environment';
 	import { AppBar } from '@skeletonlabs/skeleton';
 	import { onMount } from 'svelte';
-
 	const links = [
-		{ name: 'Projects', to: '/projects'},
+		{ name: 'Projects', to: '/projects' },
 		{ name: 'About', to: '/about' },
 		{ name: 'Contact', to: '/contact' }
 	];
 
 	let curPath: string;
 	$: {
-		if (browser && window.location.pathname) {
+		if (browser) {
 			curPath = window.location.pathname;
 			console.log(curPath);
 		}
@@ -23,17 +22,22 @@
 	// };
 </script>
 
-<div class="bg-surface-100-800-token">
+<!-- <div class="bg-surface-200-700-token"> -->
+
+<div class="bg-surface-200-700-token brightness-105">
 	<div class="container mx-auto">
-		<AppBar class="">
+		<AppBar class="!bg-inherit">
 			<svelte:fragment slot="lead">
 				<div>
-					<strong class="text-xl uppercase"><a href="/">FullStackMike</a></strong>
+					<strong
+						class="text-xl uppercase font-black bg-clip-text text-transparent bg-gradient-to-tr from-primary-700 to-secondary-500 dark:from-primary-600 dark:to-secondary-500 dark:brightness-200"
+						><a href="/">FullStackMike</a></strong
+					>
 				</div></svelte:fragment
 			>
 			<svelte:fragment slot="default">
 				<div class="flex">
-					<div class="flex-1"></div>
+					<div class="flex-1" />
 					<ul class="flex justify-center gap-8 flex-1">
 						{#each links as link}
 							<li class="font-bold uppercase">
@@ -41,28 +45,17 @@
 							</li>
 						{/each}
 					</ul>
-					<div class="flex-1 flex gap-3 justify-end">
-						<a
-							class="btn btn-sm variant-ghost-surface"
-							href="https://www.linkedin.com/in/michael-spinks-254879201/"
-							target="_blank"
-							rel="noreferrer"
-						>
-							LinkedIn
-						</a>
-						<a
-							class="btn btn-sm variant-ghost-surface"
-							href="http://github.com/mikespinks0401"
-							target="_blank"
-							rel="noreferrer"
-						>
-							GitHub
+					<div class="flex-1 flex justify-end text-sm font-semibold">
+						<a class="btn btn-sm bg-initial text-xs" href="/michael-spinks-resume.pdf" download>
+							Download Resume
 						</a>
 					</div>
 				</div>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<a class="btn btn-sm bg-primary-500 text-white"  href="/contact"> <span class="font-bold text-xl">Get In Touch</span> </a>
+				<a class="btn btn-sm variant-filled-primary text-white" href="/contact">
+					<span class="font-bold text-lg">Contact Now</span>
+				</a>
 			</svelte:fragment>
 		</AppBar>
 	</div>
@@ -70,12 +63,12 @@
 
 <style lang="postcss">
 	.active {
-		@apply text-secondary-300-600-token font-bold;
+		@apply text-primary-300-600-token font-bold;
 	}
-	ul a{
-		@apply transition duration-200
+	ul a {
+		@apply transition duration-200;
 	}
 	ul a:hover {
-		@apply text-secondary-400-500-token;
+		@apply text-primary-700;
 	}
 </style>
