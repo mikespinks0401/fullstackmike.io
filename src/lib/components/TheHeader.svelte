a146f9ed275166122d8ef69ac09cc6e8984f0cba
M.Spinks <mikespinks0401@gmail.com>
2023-05-21 19:59:28 -0500

    todo: add active state to current page

--- src/lib/components/TheHeader.svelte
+++ src/lib/components/TheHeader.svelte
@@ -1,27 +1,47 @@
 <script lang="ts">
-    import {AppBar} from "@skeletonlabs/skeleton"
+	import { browser } from '$app/environment';
+	import { AppBar } from '@skeletonlabs/skeleton';
+	import { onMount } from 'svelte';
+
 	const links = [
-		{ name: 'Home', to: '/' },
+		{ name: 'Projects', to: '/projects'},
 		{ name: 'About', to: '/about' },
 		{ name: 'Contact', to: '/contact' }
 	];
+
+	let curPath: string;
+	$: {
+		if (browser && window.location.pathname) {
+			curPath = window.location.pathname;
+			console.log(curPath);
+		}
+	}
+	// const changePath = () => {
+	// 	if (browser) {
+	// 		curPath = window.location.pathname;
+	// 	}
+	// };
 </script>
 
-
-		<div class="bg-surface-100-800-token">
-			<div class="container mx-auto">
-				<AppBar>
-					<svelte:fragment slot="lead">
-						<strong class="text-xl uppercase"><a href="/">FullStackMike</a></strong>
-					</svelte:fragment>
-					<svelte:fragment slot="default">
-						<ul class="flex justify-center gap-4 uppercase font-medium">
-							{#each links as link}
-							<li><a href={link.to}>{link.name}</a></li>	
-							{/each}
-						</ul>
-					</svelte:fragment>
-					<svelte:fragment slot="trail">
+<div class="bg-surface-100-800-token">
+	<div class="container mx-auto">
+		<AppBar class="">
+			<svelte:fragment slot="lead">
+				<div>
+					<strong class="text-xl uppercase"><a href="/">FullStackMike</a></strong>
+				</div></svelte:fragment
+			>
+			<svelte:fragment slot="default">
+				<div class="flex">
+					<div class="flex-1"></div>
+					<ul class="flex justify-center gap-8 flex-1">
+						{#each links as link}
+							<li class="font-bold uppercase">
+								<a href={link.to}>{link.name}</a>
+							</li>
+						{/each}
+					</ul>
+					<div class="flex-1 flex gap-3 justify-end">
 						<a
 							class="btn btn-sm variant-ghost-surface"
 							href="https://www.linkedin.com/in/michael-spinks-254879201/"
@@ -38,7 +58,24 @@
 						>
 							GitHub
 						</a>
-					</svelte:fragment>
-				</AppBar>
-			</div>
-		</div>
+					</div>
+				</div>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a class="btn btn-sm bg-primary-500 text-white"  href="/contact"> <span class="font-bold text-xl">Get In Touch</span> </a>
+			</svelte:fragment>
+		</AppBar>
+	</div>
+</div>
+
+<style lang="postcss">
+	.active {
+		@apply text-secondary-300-600-token font-bold;
+	}
+	ul a{
+		@apply transition duration-200
+	}
+	ul a:hover {
+		@apply text-secondary-400-500-token;
+	}
+</style>
