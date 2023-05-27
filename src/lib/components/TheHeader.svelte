1712049ca51712f46d2b80c4ae1474710f12e87b
M.Spinks <mikespinks0401@gmail.com>
2023-05-26 23:22:48 -0500

    todo: conditionally add active class to cur path

--- src/lib/components/TheHeader.svelte
+++ src/lib/components/TheHeader.svelte
@@ -1,51 +1,36 @@
 <script lang="ts">
-	import { browser } from '$app/environment';
+	import { afterNavigate } from '$app/navigation';
 	import { AppBar, drawerStore } from '@skeletonlabs/skeleton';
 	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
-	import { useRoutes } from '../../routes/homeRoutes'; 
-	import type {DrawerSettings} from "@skeletonlabs/skeleton"
-	
-	
+	import { useRoutes } from '../../routes/homeRoutes';
+	import type { DrawerSettings } from '@skeletonlabs/skeleton';
 
-	const links = useRoutes('about', 'projects', 'contact')
+	const links = useRoutes('about', 'projects', 'contact');
 
-	let name:string;
-	let windowWidth:number
-	$: if(windowWidth > 1024){
-		drawerStore.close()
-	}
-	// onMount(() => {
-	// 	window.addEventListener("resize", ()=> {
-	// 		if(window.outerWidth > 1024){
-	// 			drawerStore.close()
-	// 		}
-	// 	})
-	// 	console.log(name)
-	// });
 	let curPath: string;
-	$: {
-		if (browser) {
-			curPath = window.location.pathname;
-			console.log(curPath);
-		}
+	let windowWidth: number;
+
+	
+	$: if (windowWidth > 1024) {
+		drawerStore.close();
 	}
-	// const changePath = () => {
-	// 	if (browser) {
-	// 		curPath = window.location.pathname;
-	// 	}
-	// };
+	afterNavigate(()=>{
+		curPath = window.location.pathname
+	})
+
 	const drawerSettings: DrawerSettings = {
 		width: 'w-[280px] md:w-[480px]'
-	}
+	};
 	const openDrawer = () => {
-		drawerStore.open(drawerSettings)
-	}
+		drawerStore.open(drawerSettings);
+	};
 </script>
 
 <!-- <div class="bg-surface-200-700-token"> -->
-<svelte:window bind:outerWidth={windowWidth}/>
-<div class="bg-surface-200-700-token brightness-105">
+<svelte:window bind:outerWidth={windowWidth} />
+<!-- <div class="bg-surface-200-700-token brightness-105"> -->
+<div class="bg-transparent bg-surface-200-700-token brightness-105">
 	<!-- <div class="container mx-auto"> -->
 	<div class="px-3 mx-auto">
 		<AppBar class="!bg-inherit">
@@ -79,8 +64,10 @@
 				</div>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<button on:click={openDrawer}><i class=" md:hidden text-xl btn btn-icon fa-solid fa-bars"></i></button>
-				<a class="hidden md:block btn btn-sm variant-filled-primary text-white" href="/contact">
+				<button on:click={openDrawer}
+					><i class=" md:hidden text-xl btn btn-icon fa-solid fa-bars" /></button
+				>
+				<a class="hidden md:block btn btn-md variant-filled-primary text-white" href="/contact">
 					<span class="font-medium text-lg text-white dark:text-white">Contact Now</span>
 				</a>
 			</svelte:fragment>
