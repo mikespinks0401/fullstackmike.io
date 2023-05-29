36a98386b42ecccff4c7bc426705506c9017b321
M.Spinks <mikespinks0401@gmail.com>
2023-05-29 12:43:06 -0500

    typos

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -22,7 +22,7 @@
 				</div>
 				<div class="py-12">
 					<p>
-						Hello. My name is Michael Spinks. I've been interested in technology since I was a teenager. I built my first assembled my first gaming computer when I was either 16 or 17 years old. Building that rig taught me one of the most important values of my life, if I really wanted to do something bad enough, I can could get it done. I've tried learning programming before, it just wasn't clicking for me at the time. It wasn't until in my late 20s that I fully committed to learning. What was different? The deep desire to build SAAS products from A-Z. I have embarked on this journey of becoming a full stack developer and what follows is a little summary about myself. 
+						Hello. My name is Michael Spinks. I've been interested in technology since I was a teenager. I built my first  gaming computer when I was either 16 or 17 years old. Building that rig taught me one of the most important values of my life, if I really wanted to do something bad enough, I can get it done. I've tried learning programming before, it just wasn't clicking for me in my first two attempts. It wasn't until in my late 20s that I fully committed to learning. What was different? The deep desire to build SAAS products from A-Z. I have embarked on this journey of becoming a full stack developer and what follows is a little summary about my learning journey. 
 					</p>
 					<p>
 						Over the course of my four years of learning, I have acquired a diverse set of
@@ -32,7 +32,7 @@
 						frameworks that have shaped my skill set. Although I do not believe that I am a expert in any one area, I feel comfortable enough to dive into whatever part of the stack is necessary to get things moving forward.
 					</p>
 					<p>
-						What I enjoy most about programming isthe challenges that come with
+						What I enjoy most about programming are the challenges that come with
 						learning new technologies. I remember spending a good chucnk of time wrapping my head around the
 						complexities of implementing an authentication system with JWT in Nuxt 3. I am proud of the fact that I can now confidently implement an auth system with a back-end language and JWT or whatever technology is required. Through
 						perseverance and the ability to slow down and think critically, I have developed
@@ -40,13 +40,13 @@
 					</p>
 					<p>
 						While I appreciate both front-end and back-end development, I find myself more
-						drawn to the backend, as it is where the business logic is implemented. I do welcome and enjoy working through the challenges that come from either part of the stack. The end goal is never to simply get the necessary answer, it will always be to gain understanding.
+						drawn to the backend, as it is where the business logic is implemented.However, I welcome and enjoy working through the challenges that come from either part of the stack. The end goal is never to simply get the necessary answer; it will always be to gain understanding.
 					</p>
 					<p>
 						While I have pursued my programming journey solo, I am eager to
 						collaborate and learn from other developers in a team enviroment. Being part of an awesome team of passionate
 						individuals is one of my future goals as developer. Additionally, I aspire
-						to dive deeper into Svelte and Go, sharpen my skills in TDD (Test-Driven Development),
+						to dive deeper into Svelte and Go, sharpen my skills in Test-Driven Development(TDD),
 						and deliver top-quality software.
 					</p>
 					<p>
@@ -71,7 +71,7 @@
 						Looking ahead, I am excited about continuous learning and growth in the programming
 						field. One of my contributions to the community will be sharing tutorials that assist
 						aspiring developers on their learning journey, as I believe in giving back and helping
-						others succeed.
+						others succeed. I hope to inspire and assist those that are crazy enough to take this serous. This journey has been one hell of a ride, but I wouldn't trade it for the world.
 					</p>
 				</div>
 			</div>
