6adb347f47a59f5558a806e7b2a152ef12dfa77e
M.Spinks <mikespinks0401@gmail.com>
2023-05-29 11:32:19 -0500

    refactor: Text Edit

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -22,41 +22,36 @@
 				</div>
 				<div class="py-12">
 					<p>
-						Hello there. My name is Michael Spinks. I have always had a deep interest in
-						programming, and although I didn't fully commit to learning until my late 20s,
-						technology has been a passion of mine since I assembled my first computer at the age of
-						17. The desire to have the knowledge to create my own SAAS product drove me to pursue a
-						career as a full stack developer.
+						Hello. My name is Michael Spinks. I've been interested in technology since I was a teenager. I built my first assembled my first gaming computer when I was either 16 or 17 years old. Building that rig taught me one of the most important values of my life, if I really wanted to do something bad enough, I can could get it done. I've tried learning programming before, it just wasn't clicking for me at the time. It wasn't until in my late 20s that I fully committed to learning. What was different? The deep desire to build SAAS products from A-Z. I have embarked on this journey of becoming a full stack developer and what follows is a little summary about myself. 
 					</p>
 					<p>
 						Over the course of my four years of learning, I have acquired a diverse set of
-						programming languages and technologies. From PHP, HTML, CSS, and JavaScript to React,
+						programming languages and technologies. From PHP(where I learned the fundamentals of programming), HTML, CSS, and JavaScript to Linux, React,
 						Tailwind, Vue 3, Nuxt 3, Node, Nginx, TypeScript, Git, MySQL, Cypress, GitHub Actions,
 						JWT, Vuetify 3, Go, Svelte, and SvelteKit, I have explored a wide range of tools and
-						frameworks that have shaped my skill set.
+						frameworks that have shaped my skill set. Although I do not believe that I am a expert in any one area, I feel comfortable enough to dive into whatever part of the stack is necessary to get things moving forward.
 					</p>
 					<p>
-						One of the most rewarding aspects of programming is facing the challenges that come with
-						adopting new technologies. I vividly remember spending two weeks unraveling the
-						complexities of implementing an authentication system with JWT in Nuxt 3. Through
+						What I enjoy most about programming isthe challenges that come with
+						learning new technologies. I remember spending a good chucnk of time wrapping my head around the
+						complexities of implementing an authentication system with JWT in Nuxt 3. I am proud of the fact that I can now confidently implement an auth system with a back-end language and JWT or whatever technology is required. Through
 						perseverance and the ability to slow down and think critically, I have developed
 						problem-solving skills that enable me to tackle any obstacle.
 					</p>
 					<p>
-						While I appreciate both front-end and back-end development, I find myself particularly
-						drawn to the backend, as it is where the magic happens—the intricate mechanisms behind
-						the scenes that power applications and make them come alive.
+						While I appreciate both front-end and back-end development, I find myself more
+						drawn to the backend, as it is where the business logic is implemented. I do welcome and enjoy working through the challenges that come from either part of the stack. The end goal is never to simply get the necessary answer, it will always be to gain understanding.
 					</p>
 					<p>
-						While I have primarily pursued my programming journey independently, I am eager to
-						collaborate and learn from other developers. Being part of an awesome team of passionate
-						programmers is one of my future goals as a full stack developer. Additionally, I aspire
+						While I have pursued my programming journey solo, I am eager to
+						collaborate and learn from other developers in a team enviroment. Being part of an awesome team of passionate
+						individuals is one of my future goals as developer. Additionally, I aspire
 						to dive deeper into Svelte and Go, sharpen my skills in TDD (Test-Driven Development),
 						and deliver top-quality software.
 					</p>
 					<p>
 						To stay up to date with the latest trends, I follow various tech-related YouTube
-						channels that keep me informed and inspired. When learning a new technology, I start by
+						channels that keep me informed on the latest news. When learning a new technology, I start by
 						watching recent videos to familiarize myself with its practical applications. I then
 						dive into the documentation, supplementing my learning with targeted Google searches
 						when needed.
@@ -65,7 +60,7 @@
 						One project that holds personal significance to me is a fitness application I developed
 						in 2020 using JavaScript and a strap-on heart rate monitor. This project gained
 						importance when my family was impacted by COVID, and we became more focused on our heart
-						health. I hope to recreate the app with an enhanced design to further promote wellness.
+						health. I intend on recreating the app with an enhanced design to further promote wellness. 
 					</p>
 					<p>
 						As a self-taught developer, I take pride in my ability to overcome challenges and find
