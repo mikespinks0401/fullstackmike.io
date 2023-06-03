ab35253a55ff71ae96d47c0de2bcddb483b4482e
M.Spinks <mikespinks0401@gmail.com>
2023-06-02 19:34:15 -0500

    TODO: add validation on backend

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -3,10 +3,10 @@ import { env } from "$env/dynamic/private";
 import { dev } from "$app/environment";
 import { sendMail } from "$lib/server/mailer";
 import type { SendMailInfo } from "$lib/server/services/zoho-api";
-//       import type { SendMailInfo } from "$lib/server/mailer";
-import { fail } from "@sveltejs/kit";
 
 const Secret = dev == true ? "1x0000000000000000000000000000000AA" : env.TURNSTILE_SECRET_KEY
+const EMAIL_API_KEY = env.CF_MAIL_WORKER_API_KEY
+const WORKERS_URL = env.CF_WORKERS_URL
 export const actions = {
     default: async( event ) => {
         const data = await event.request.formData();
@@ -15,18 +15,11 @@ export const actions = {
         const phoneNumber = data.get('form-phoneNumber')
         const subject = data.get('form-subject')
         const msg = data.get('form-msg') 
-        const ZOHO_CLIENT_ID = env.ZOHO_API_CONTACT_FORM_CLIENT_ID_CLIENT
-        const ZOHO_CLIENT_SECRET = env.ZOHO_API_CONTACT_FOM_CLIENT_SECRET
         
-        const emailText = `${name} 
-        ${email}
-        ${phoneNumber ? phoneNumber : ''}
-        
-        
-        ${msg}`
+        const emailText = `<p>${name}</p><p>${email}</p><p>${phoneNumber ? phoneNumber : ''}</p><p></p><p></p> <pre>${msg}</pre>`
         const mailInfo:SendMailInfo = {
-            from: env.EMAIL_EMAIL,
-            to: env.EMAIL_EMAIL,
+            fromAddress: env.EMAIL_EMAIL,
+            toAddress: env.EMAIL_EMAIL,
             subject: `Form Submission - ${subject}`,
             content: emailText
 
@@ -52,9 +45,18 @@ export const actions = {
         //show success even if email is not sent??
         //send email if outcome is successful
         if(outcome.success){
-            sendMail(mailInfo)
-            // await SendMail(mailInfo)
+           let res = await fetch(WORKERS_URL,{
+                headers:{
+                    "Authorization": `Bearer ${EMAIL_API_KEY}`
+                }
+           })
+           let {accessToken} = await res.json()
+           let {success} = await sendMail(accessToken,mailInfo)
+           if(!success == false){
 
+            console.log("Error Sending Email - " + mailInfo)
+
+           }
         }else {
             return {success: true}
         }
