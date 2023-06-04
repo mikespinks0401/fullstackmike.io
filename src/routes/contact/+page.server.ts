284b766ac375f5ebd81429d1465a607f90fb43a3
M.Spinks <mikespinks0401@gmail.com>
2023-06-04 00:39:47 -0500

    added: missing validation todo: validate email, too large, text?

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -1,4 +1,5 @@
 import type { Actions } from "./$types"
+import { fail } from "@sveltejs/kit";
 import { env } from "$env/dynamic/private";
 import { dev } from "$app/environment";
 import { sendMail } from "$lib/server/mailer";
@@ -18,13 +19,45 @@ const EMAIL_API_KEY = env.CF_MAIL_WORKER_API_KEY
 const WORKERS_URL = env.CF_WORKERS_URL
 export const actions = {
     default: async( event ) => {
+
+        let missingData:string[] = [] 
         const data = await event.request.formData();
         const name = data.get('form-name')
         const email = data.get('form-email') 
         const phoneNumber = data.get('form-phoneNumber')
         const subject = data.get('form-subject')
         const msg = data.get('form-msg') 
-      
+        const requiredFields = [name, email, subject, msg]
+
+        requiredFields.forEach((field, index) => {
+            if(!hasValue(field)){
+                let value
+                switch(index){
+                    case 0:
+                        value = 'name'
+                        break
+                    case 1:
+                        value = 'email'
+                        break
+                    case 2:
+                        value = 'subject'
+                        break
+                    case 3:
+                        value = "msg"
+                        break
+                }
+                if(value == null){
+                    return
+                }
+                missingData.push(value)
+            }
+        })
+
+        if(missingData.length > 0){
+            return fail(404, {error: true, missingData: (missingData.length == 0 ? [] : missingData)})
+        }
+        //REMOVE WHEN DONE VALIDATING
+        // return
         const emailText = `<p>${name}</p><p>${email}</p><p>${phoneNumber ? phoneNumber : ''}</p><p></p><p></p> <pre>${msg}</pre>`
         const mailInfo:SendMailInfo = {
             fromAddress: env.EMAIL_EMAIL,
@@ -36,23 +69,14 @@ export const actions = {
         const token = data.get('cf-turnstile-response')
         const ip = event.getClientAddress()
 
-
+        if(Secret === null || token == null){
+            return
+        }
         //turnstile server response validation form
-        let formData = new FormData()
-
-        formData.append('secret', Secret)
-        formData.append('response', token!)
-        formData.append('remoteip', ip!)
-        const url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
-        const result = await fetch(url, {
-            body: formData,
-            method: 'POST'
-        })
-        const outcome = await result.json()
-
+        const outcome = await validateCFToken(Secret,token.toString(),ip)
         //show success even if email is not sent??
         //send email if outcome is successful
-        if(outcome.success){
+        if(outcome?.success){
            let res = await fetch(WORKERS_URL,{
                 headers:{
                     "Authorization": `Bearer ${EMAIL_API_KEY}`
@@ -71,3 +95,23 @@ export const actions = {
         return { success: true}
     }
 } satisfies Actions;
+
+function hasValue(formKey: FormDataEntryValue | null){
+    console.log(`Checking ${formKey?.toString()}`)
+    return   (!formKey || formKey?.toString().trim().length === 0) ? false : true
+}
+
+async function validateCFToken(secret: string, token:string, ip: string){
+        let formData = new FormData()
+
+        formData.append('secret', Secret)
+        formData.append('response', token!)
+        formData.append('remoteip', ip!)
+        const url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
+        const result = await fetch(url, {
+            body: formData,
+            method: 'POST'
+        })
+        const outcome = await result.json()
+        return outcome
+}
