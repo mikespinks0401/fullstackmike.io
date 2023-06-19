53337e30b4ed4bc136fd8b9d861b0d5d0a1accf6
M.Spinks <mikespinks0401@gmail.com>
2023-06-18 18:01:42 -0700

    change: hero and services

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,23 +37,28 @@
 
 	<section class="w-full">
 		<div
-			class=" container mx-auto max-w-7xl p-4 grid grid-cols-1 sm:px-8 md:px-16 md:grid-cols-2 md:mt-16 md:mb-12"
+			class=" container mx-auto max-w-7xl p-4 grid grid-cols-1 sm:px-0  md:grid-cols-2 md:mt-16 md:mb-12"
 		>
 			<div
-				class="py-4 grid-cols-1 flex flex-col gap-2 min-h-full relative justify-center md:bottom-6 max-w-2xl"
+				class="py-8  md:py-4 grid-cols-1 flex flex-col gap-2 min-h-full relative justify-center md:bottom-6 max-w-2xl px-4"
 			>
-				<h1 class=" !text-3xl md:!text-6xl tracking font-black pb-3" data-test="hero-title">
+				<h1 class=" !text-4xl lg:!text-6xl tracking font-black " data-test="hero-title">
 					<span
 						class="bg-clip-text bg-gradient-to-tr text-transparent from-yellow-500 dark:from-yellow-300 to-orange-700 dark:to-orange-500"
 						>Ignite</span
 					> Your Web Presence 🔥
 				</h1>
-				<h4 class="hidden md:block md:text-xl !opacity-70 font-semibold tracking-tighter" data-test="hero-subtitle">
+				<h4
+					class="md:block md:text-xl !opacity-70 font-semibold tracking-tighter"
+					data-test="hero-subtitle"
+				>
 					Michael Spinks - Your Full Stack Developer
 				</h4>
 				<p data-test="hero-description" class="opacity-50">
-					Elevate your online presence with a fast, modern, and user-friendly application that will surpass client demands				</p>
-				<div class="flex flex-col gap-4 pt-8 mg:pt-6 md:flex-row">
+					Elevate your online presence with a fast, modern, and user-friendly application that will
+					surpass client demands
+				</p>
+				<div class=" flex flex-col gap-4 pt-8 md:pt-6 md:flex-row">
 					<a class="btn btn-lg variant-filled-primary !text-white font-black" href="/contact"
 						>Contact Now</a
 					>
@@ -70,30 +75,31 @@
 				<!-- Hero Img -->
 				<div class="flex flex-col justify-center space-x-2 items-center">
 					<figure class="">
-						<div class="w-72 h-72 lg:w-96 lg:h-96 pt-16 overflow-hidden relative border  rounded-full md:border-none md:overflow-visible">
-						<section class="img-bg relative overflow-hidden" />
+						<div
+							class="w-72 h-72 lg:w-96 lg:h-96 pt-16 overflow-hidden relative border rounded-full md:border-none md:overflow-visible"
+						>
+							<section class="img-bg relative overflow-hidden" />
 							<img
 								src="/michael-spinks-developer.png"
 								alt="Michael Spinks Full Stack Develoepr"
-								class="scale-150 md:scale-110 -bottom-40 md:-bottom-20  lg:-bottom-16 right-4 absolute origin-center"
+								class="scale-150 md:scale-110 -bottom-40 md:-bottom-20 lg:-bottom-16 right-4 absolute origin-center"
 							/>
 						</div>
 					</figure>
-					<!-- / -->
 				</div>
 			</div>
 		</div>
 	</section>
 	<!--End Hero Section-->
-	<section class="bg-surface-300-600-token brightness-110 w-full z-10 min-h-[320px] py-8">
-		<div class="container mx-auto flex flex-col px-4 lg:px-0">
+	<section class="bg-surface-300-600-token brightness-110 w-full min-h-[320px] py-8">
+		<div class="container mx-auto flex flex-col px-4 max-w-7xl lg:px-0">
 			<h2 class="text-center text-2xl font-semibold md:py-12">Services</h2>
 			<!--Services Section-->
 			<div class="flex flex-col md:flex-row justify-around gap-2">
 				{#each services as { name, description, icon }, i}
 					<div class="flex flex-col gap-1 md:gap-4 max-w-2xl py-4">
 						<div class="flex flex-col gap-1 md:gap-2">
-							<h2 class="font-black text-2xl md:text-6xl text-primary-500"><i class={icon} /></h2>
+							<h2 class="font-black text-4xl md:text-6xl text-primary-500"><i class={icon} /></h2>
 							<h4 class="font-bold text-md md:text-xl">{name}</h4>
 						</div>
 						<p class="opacity-80 max-w-md">{description}</p>
