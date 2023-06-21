e2b1ef7448e3e7a61cf2e9af8bcc70e09bcc00df
M.Spinks <mikespinks0401@gmail.com>
2023-06-20 17:24:56 -0700

    created: projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,10 +1,64 @@
+<script lang="ts">
+	type Project = {
+		name: string;
+		description: string;
+		href: string;
+		img: string;
+		hovered: boolean;
+	};
+
+
+	const projects: Project[] = [
+		{
+			name: 'Dedicated To Your Freight',
+			description:
+				'Wordpress Site Redesign with performance boosts. Improved load time by over 75%.',
+			href: 'https://www.dedicatedtoyourfreight.com',
+			img: 'project-D2F.png',
+			hovered: false
+
+		},
+		{
+			name: 'Rest Api Country Picker',
+			description: 'Built with Nuxt 3 and deployed on github pages',
+			href: 'https://mikespinks0401.github.io/project-restApi-countries/',
+			img: 'project-countries.png',
+			hovered: false
+		},
+		{
+			name: 'News Home Page Responsive Design',
+			description:
+				'A responsive homepage design built with tailwindCSS and Nuxt 3, deployed with github pages',
+			href: 'https://mikespinks0401.github.io/project-news-homepage-layout/',
+			img: 'project-newsHomePage.png',
+			hovered: false
+		}
+	];
+</script>
+
 <svelte:head>
 	<title>Projects</title>
 </svelte:head>
 <div>
 	<div class="container mx-auto">
-		<div class="flex flex-col justify-center">
-			<h1 class="text-4xl text-center pt-4">Projects</h1>
+		<div class="flex flex-col justify-center text-center py-12">
+			<h1 class="text-4xl">Projects</h1>
+			<p >(Click On A Project To View)</p>
+		</div>
+		<div class="w-full grid md:grid-cols-3 gap-20 p-3">
+			{#each projects as project, index}
+				<a on:mouseover={()=> project.hovered = true} on:focus={() =>project.hovered = true} on:blur={()=>project.hovered = false} on:mouseleave={()=> project.hovered = false} href={project.href} target="_blank" class="hover:shadow-md h-auto  md:max-w-sm">
+					<div class=" border h-full {project.hovered === true ? 'bg-surface-hover-token': 'bg-surface-300-600-token'}">
+						<div class="overflow-hidden">
+							<img class=" object-contain transition duration-200 {project.hovered === true ? 'scale-125' : ''}"  src="/{project.img}" alt={project.name} />
+						</div>
+						<div class="p-4">
+							<p class="font-bold text-center">{project.name}</p>
+							<p class="py-4 text-opacity-50">{project.description}</p>
+						</div>
+					</div>
+				</a>
+			{/each}
 		</div>
 	</div>
 </div>
