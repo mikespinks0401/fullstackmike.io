e7fa168df09bf454dbb2bc80e42971602bf33429
M.Spinks <mikespinks0401@gmail.com>
2025-03-03 00:12:12 -0800

    Less Is More

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -22,57 +22,9 @@
 				</div>
 				<div class="py-12 text-xs leading-5">
 					<p>
-						Hello. My name is Michael Spinks.👋 I built my first  gaming computer when I was either 16 or 17 years old and have always had a passion for technology ever since.💻🎮 Building that rig taught me that if I really wanted to do something bad enough, I can get it done. It wasn't until in my late 20s that I fully committed to learning how to program. What sparked that? The deep desire to possess the knowledge to build a SAAS products from A-Z solo.💡 I have embarked on this journey of becoming a full stack developer and what follows is a little summary of how everything unfolded. 
+						Hello. My name is Michael Spinks.👋 I have been building web applications since 2019.
 					</p>
-					<p>
-						Over the course of my four years of learning, I have acquired a diverse set of
-						programming languages and technologies. My journey started with <strong>PHP(where I learned the fundamentals of programming), HTML, CSS, and JavaScript. I then expanded my knowledge to include Linux(through a Raspberry Pi), React,
-						Tailwind, Vue 3, Nuxt 3, Node, Nginx, TypeScript, Git, MySQL, Cypress, GitHub Actions,
-						JWT, Vuetify 3, Go, Svelte, and SvelteKit</strong>. I have explored a wide range of tools and
-						frameworks that have shaped my skill set. Although I do not consider myself an expert in any one area, I feel comfortable enough to dive into whatever part of the stack is necessary to get things moving forward and bring an idea to life.
-					</p>
-					<p>
-							What I enjoy most about programming are the challenges that come with
-						learning new technologies. I remember spending a good chucnk of time wrapping my head around the
-						complexities of implementing an authentication system with JWT in Nuxt 3. I am proud of the fact that I can now confidently implement an auth system with a back-end language and JWT or whatever technology is required. Through
-						perseverance and the ability to slow down and think critically, I have developed
-						problem-solving skills that enable me to tackle any obstacle.🧠💡
-					</p>
-					<p>
-						While I appreciate both front-end and back-end development, I find myself more
-						drawn to the backend, as it is where the business logic is implemented.However, I welcome and enjoy working through the challenges that come from either part of the stack. The end goal is never to simply get the necessary answer; it will always be to gain understanding.
-					</p>
-					<p>
-						While I have pursued my programming journey solo, I am eager to
-						collaborate and learn from other developers in a team enviroment. Being part of an awesome team of passionate
-						individuals is one of my future goals as developer. Additionally, I aspire
-						to dive deeper into Svelte and Go, sharpen my skills in Test-Driven Development(TDD),
-						and deliver top-quality software.🤝💡🚀
-					</p>
-					<p>
-						To stay up to date with the latest trends, I follow various tech-related YouTube
-						channels that keep me informed on the latest news. When learning a new technology, I start by
-						watching recent videos to familiarize myself with its practical applications. I then
-						dive into the documentation, supplementing my learning with targeted Google searches
-						when needed.
-					</p>
-					<p>
-						One project that holds personal significance to me is a fitness application I developed
-						in 2020 using JavaScript and a strap-on heart rate monitor. This project gained
-						importance when my family was impacted by COVID, and we became more focused on our heart
-						health. I intend on recreating the app with an enhanced design to further promote wellness. 
-					</p>
-					<p>
-						As a self-taught developer, I take pride in my ability to overcome challenges and find
-						solutions independently. My dedication to delivering secure and robust code sets me
-						apart, as I consistently strive to ensure that my work is free of vulnerabilities.
-					</p>
-					<p>
-						Looking ahead, I am excited about continuous learning and growth in the programming
-						field. One of my contributions to the community will be sharing tutorials that assist
-						aspiring developers on their learning journey, as I believe in giving back and helping
-						others succeed. I hope to inspire and assist those that are crazy enough to take this serous. This journey has been one hell of a ride, but I wouldn't trade it for the world.
-					</p>
+					<p>I'm a chill guy that looks forward to the next project. Reach out for more info.</p>
 				</div>
 			</div>
 		</section>
